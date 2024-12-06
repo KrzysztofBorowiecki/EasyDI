727678b527ecaa6f725fc7c6391e992a44d8568f
krzysztofborowiecki <krzysztof.borowiecki58@gmail.com>
2024-12-06 11:27:56 +0100

    feat: add IContainer interface and ContainerExtensions with AttachSingleton methods

---
+++ src/EasyDI/ContainerExtensions.cs
@@ -0,0 +1,18 @@
+namespace EasyDI;
+
+public static class ContainerExtensions
+{
+    public static IContainer AttachSingleton(this IContainer container, Type typeToRegister,
+        object implementation)
+    {
+        //TODO: write an implementation
+        throw new NotImplementedException();
+    }
+
+    public static IContainer AttachSingleton(this IContainer container, Type typeToRegister,
+        Type implementationType)
+    {
+        //TODO: write an implementation
+        throw new NotImplementedException();
+    }
+}
---
+++ src/EasyDI/IContainer.cs
@@ -0,0 +1,6 @@
+namespace EasyDI;
+
+public interface IContainer
+{
+    
+}
