192ae573623b2070086205f75c2d87c592988d12
krzysztofborowiecki <krzysztof.borowiecki58@gmail.com>
2024-12-06 13:02:05 +0100

    feat: added Register method

--- src/EasyDI/Container.cs
+++ src/EasyDI/Container.cs
@@ -2,11 +2,19 @@ namespace EasyDI;
 
 public interface IContainer : IDisposable
 {
-    
+    void Register(Type registerType, Type implementationType, Func<object> factory, LifeTime lifeTime);
 }
 
 public class Container : IContainer
 {
+    private readonly Dictionary<Type, Func<object>> _registeredDependencies = new();
+
+    public void Register(Type registerType, Type implementationType, Func<object> factory, LifeTime lifeTime)
+    {
+        //TODO: write an implementation
+        //   throw new NotImplementedException();
+    }
+
     public void Dispose()
     {
         // TODO: release managed resources here
