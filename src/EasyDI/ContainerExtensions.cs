e501d38ba079294ff21cf6fdd9cb85c3a73bd19c
krzysztofborowiecki <krzysztof.borowiecki58@gmail.com>
2024-12-06 13:03:28 +0100

    feat: implement AttachSingleton extensions methods

--- src/EasyDI/ContainerExtensions.cs
+++ src/EasyDI/ContainerExtensions.cs
@@ -5,15 +5,20 @@ public static class ContainerExtensions
     public static IContainer AttachSingleton(this IContainer container, Type typeToRegister,
         object implementation)
     {
-        //TODO: write an implementation
-        throw new NotImplementedException();
+        ArgumentNullException.ThrowIfNull(container);
+        ArgumentNullException.ThrowIfNull(typeToRegister);
+        ArgumentNullException.ThrowIfNull(implementation);
+
+        container.Register(typeToRegister, implementation.GetType(), () => implementation,
+            LifeTime.Singleton);
+        return container;
     }
 
     public static IContainer AttachSingleton(this IContainer container, Type typeToRegister,
         Type implementationType)
     {
-        //TODO: write an implementation
-        throw new NotImplementedException();
+        container.Register(typeToRegister, implementationType, null, LifeTime.Singleton);
+        return container;
     }
 
     public static T Resolve<T>(this IContainer container)
